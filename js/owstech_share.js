54446748698a4f255c41f9e4fa4d34c2949ba7ea
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-12-22 05:01:20 -0800

    updated js to pop window in a floating resized popup instead of a new tab/window

--- js/owstech_share.js
+++ js/owstech_share.js
@@ -1,7 +1,15 @@
 Drupal.behaviors.owstech_share = {
 
 	attach: function(context, settings){
-        console.log( 'Drupal.behaviors.owstech_share', context, settings );
-    }
+
+    console.log( 'Drupal.behaviors.owstech_share', context, settings );
+    jQuery( '.drawer ul li a' ).click( function( e ){
+      console.log( arguments );
+      var href = jQuery( this ).attr('href');
+      window.open( href, 'owstech_share_popup', 'width=450,height=350,resizeable=1,');
+      e.preventDefault();
+    });
+
+  }
 
 };
