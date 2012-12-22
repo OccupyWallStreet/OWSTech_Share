df7229466b67f9030d5da8a6829797496282bec2
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-12-22 05:42:43 -0800

    tweaked popup window sizes

--- js/owstech_share.js
+++ js/owstech_share.js
@@ -1,12 +1,39 @@
 Drupal.behaviors.owstech_share = {
 
 	attach: function(context, settings){
-
-    console.log( 'Drupal.behaviors.owstech_share', context, settings );
+    if ( console && console.log ) console.log( 'Drupal.behaviors.owstech_share', context, settings );
     jQuery( '.drawer ul li a' ).click( function( e ){
-      console.log( arguments );
-      var href = jQuery( this ).attr('href');
-      window.open( href, 'owstech_share_popup', 'width=450,height=350,resizeable=1,');
+      var href, className, win_title, w, h;
+      href = jQuery( this ).attr('href');
+      className = jQuery( this ).attr('class');
+      switch ( className ) {
+        case 'icon facebook':
+          win_title = 'share_facebook';
+          w = 450;
+          h = 290;
+        break;
+        case 'icon twitter':
+          win_title = 'share_twitter';
+          w = 450;
+          h = 380;
+        break;
+        case 'icon googleplus':
+          win_title = 'share_google-plus';
+          w = 475;
+          h = 180;
+        break;
+        case 'icon reddit':
+          win_title = 'share_reddit';
+          w = 450;
+          h = 500;
+        break;
+        default:
+          win_title = 'share_popup';
+          w = 450;
+          h = 290;
+        break;
+      } 
+      window.open( href, win_title, 'width=' + w + ',height=' + h + ',resizeable=1,status=false,toolbar=false,location=false');
       e.preventDefault();
     });
 
